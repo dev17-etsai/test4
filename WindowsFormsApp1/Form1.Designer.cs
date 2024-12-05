55dd328ba09e1559c588903a5a8fdd7f0b15e9ba
dev17-etsai <dev17@etsai.mx>
2024-12-05 13:13:24 -0600

    version 10

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(150, 59);
             this.button1.TabIndex = 0;
-            this.button1.Text = "version 4";
+            this.button1.Text = "version 10";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
